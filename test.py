6716f3e4ffed18c217eecc85fea2160f3ee59b41
spline <reticulatingspline@github.com>
2014-10-12 08:22:53 -0400

    Continue to add tests.

--- test.py
+++ test.py
@@ -11,7 +11,12 @@ class CFBTestCase(PluginTestCase):
     plugins = ('CFB',)
     
     def testCFB(self):
+        # cfbarrests, cfbbowls, cfbcoachsalary, cfbconferences, cfbcountdown, cfbgamestats, cfbheisman,
+        # cfbheismanvoting, cfbinjury, cfbplayerinfo, cfbplayoffcountdown, cfbpolls,
+        # cfbpowerrankings, cfbrankings, cfbroster, cfbschedule, cfbstandings, cfbstats,
+        # cfbteaminfo, cfbteamleaders, cfbteams, cfbteamstats, cfbtv, cfbweeklyleaders,
+        # spurrier
         self.assertNotError('cfbarrests')
-
+        self.assertRegexp('cfbbowls 2011 orange', 'West Virginia 70 \- Clemson 33')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
