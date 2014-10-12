a44170327a542c8d457b316416091ef2f65f5a98
spline <reticulatingspline@github.com>
2014-10-12 08:56:37 -0400

    Update tests.

--- test.py
+++ test.py
@@ -12,11 +12,28 @@ class CFBTestCase(PluginTestCase):
     
     def testCFB(self):
         # cfbarrests, cfbbowls, cfbcoachsalary, cfbconferences, cfbcountdown, cfbgamestats, cfbheisman,
-        # cfbheismanvoting, cfbinjury, cfbplayerinfo, cfbplayoffcountdown, cfbpolls,
-        # cfbpowerrankings, cfbrankings, cfbroster, cfbschedule, cfbstandings, cfbstats,
+        # cfbheismanvoting, cfbinjury, cfbplayerinfo, cfbpolls,
+        # cfbrankings, cfbroster, cfbschedule, cfbstandings, cfbstats,
         # cfbteaminfo, cfbteamleaders, cfbteams, cfbteamstats, cfbtv, cfbweeklyleaders,
         # spurrier
         self.assertNotError('cfbarrests')
         self.assertRegexp('cfbbowls 2011 orange', 'West Virginia 70 \- Clemson 33')
+        self.assertNotError('cfbcoachsalary')
+        self.assertResponse('cfbconferences', 'Valid CFB Conferences: AAC | ACC | BIG10 | BIG12 | IA | MAC | MWC | PAC12 | SEC | SUN | USA')
+        self.assertNotError('cfbcountdown')
+        self.assertNotError('cfbgamestats Oregon State')
+        self.assertNotError('cfbpolls ap 2007 2')
+        self.assertNotError('cfbpolls bcs 2007 2')
+        self.assertNotError('cfbpolls ap')
+        self.assertNotError('cfbpolls usatoday')
+        # self.assertNotError('cfbroster Alabama')  # broken.
+        self.assertNotError('cfbschedule Alabama')
+        self.assertNotError('cfbstandings sec')
+        self.assertNotError('cfbstats rushing')
+        self.assertNotError('cfbstats points')
+                            
+        
+        
+        
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
